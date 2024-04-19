946523c0194e7ca9e32f29f01332e2087e0e10ab
Sandra de Wijs <s.dewijs@tilburguniversity.edu>
2024-04-19 16:32:19 +0200

    Regels toegevoeg

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,5 +1,8 @@
 prijzen = {
-    'Aardbei':'3',
-    'Vanille': '4', 
-    'Chocolade': '5'
+    'Aardbei':3,
+    'Vanille': 4, 
+    'Chocolade': 5
 }
+aanbieding= (3*0.8)
+print(aanbieding)
+recalme_tekst= 'Vandaag in de aanbieding: Aardbei-ijs, 1 liter - slechts € <aanbieding>'
